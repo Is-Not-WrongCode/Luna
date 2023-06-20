3a624b8a2e948f93f5552e3b6b83500d5b9907cd
보경 <97583162+bokoo14@users.noreply.github.com>
2023-06-20 17:17:31 +0900

    chore: ADView 위치 조정

--- CarrotMarket/CarrotMarket/View/AD/AdView.swift
+++ CarrotMarket/CarrotMarket/View/AD/AdView.swift
@@ -12,9 +12,17 @@ struct AdView: View {
         VStack (spacing: 0){
             NavigationLink {
                 // Destination
+                Text("Destinaton View")
             } label: {
-                Text("무더운 여름, 미리 대비해요.")
-                    .frame(maxWidth: .infinity, alignment: .leading)
+                HStack {Text("무더운 여름, 미리 대비해요.")
+                        .font(.system(size: 18, weight: .bold))
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    Spacer()
+                    Image(systemName: "chevron.right")
+                }
+                .frame(height: 70)
+                .border(.red)
+                
             } // NavigationLink
             
             ScrollView(.horizontal, showsIndicators: false) {
