ed59236eb7e223d7b522ebc2f18aa3ff7cd5496b
보경 <97583162+bokoo14@users.noreply.github.com>
2023-07-06 15:45:17 +0900

    style: NavView 폰트 색상 및 padding값 수정

--- Webtoon/Webtoon/WeebtoonsNav/WeebtoonNavView.swift
+++ Webtoon/Webtoon/WeebtoonsNav/WeebtoonNavView.swift
@@ -14,19 +14,17 @@ struct WeebtoonNavView: View {
     
     var body: some View {
         VStack(alignment: .leading, spacing: 0) {
-            
                 navigationLinkCell(cellTitle: "보경님, 이 웹툰들\n최신 이야기를 놓치고 계신 것 같아요!") {
                     Text("네비게이션 링크")
                 }
-            
-            .tint(.black)
-            
             ForEach(0..<3) { index in
                 webtoonAD(imageTitle: imageTitle[index], webtoonTitle: weebtoonTitle[index], webtoonMaker: weebtoonMaker[index])
             }
-           
+            .padding(.horizontal, 16)
+            
+            //.border(.red)
         } // VStack
-        .padding(.horizontal, 16)
+        .background(Color("BgDarkGray"))
     }
     
     // 단순히 NavigationLink만 있는 Cell
@@ -39,12 +37,16 @@ struct WeebtoonNavView: View {
                 Text(cellTitle)
                     .multilineTextAlignment(.leading)
                     .foregroundColor(.white)
+                    .padding(.leading, 15)
                 Spacer()
+                Image(systemName: "chevron.right")
+                    .foregroundColor(.white)
+                    .frame(width: 8, height: 14)
+                    .padding(.trailing, 12)
             }
-            .frame(maxWidth: .infinity)
+            //.frame(maxWidth: .infinity)
             .frame(height: 80)
-            .border(.red)
-            
+//            .border(.red)
         }
     } //: ViewBuilder - NavigationLinkCell
     
@@ -59,22 +61,23 @@ struct WeebtoonNavView: View {
             VStack (alignment: .leading, spacing: 0){
                 Text("무료 충전 완료!")
                     .font(.system(size: 12, weight: .bold))
-                    .foregroundColor(Color("mainColor"))
+                    .foregroundColor(Color("FontGreen"))
                     .padding(.bottom, 3)
                     
                 Text(webtoonTitle)
                     .font(.system(size: 13, weight: .regular))
+                    .foregroundColor(.white)
                     .padding(.bottom, 2)
                 
                 Text(webtoonMaker)
                     .font(.system(size: 11, weight: .light))
+                    .foregroundColor(.white)
             } // VStack
             
             Spacer()
         } // HStack
         .padding(.bottom, 10)
     } // ViewBuilder - webtoonAD
-    
 }
 
 struct WeebtoonNavView_Previews: PreviewProvider {
